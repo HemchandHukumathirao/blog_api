9e639b40c2031a548caf204838f03aec22289679
Hemchand Hukumathirao <hemchand.hukumathirao@eposnow.com>
2023-04-11 00:14:38 +0100

    project set up

---
+++ src/main/java/com/blog/entities/User.java
@@ -0,0 +1,16 @@
+﻿package com.blog.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String about;
+}
